5c97843ee0083319da7e054f111c64e1b335eb91
nacho914 <39324242+nacho914@users.noreply.github.com>
2020-01-15 21:49:15 -0700

    Se modifica diseño del agregado del equipo, ademas que se realizan los inserts a la tabla jugadores.

--- Prop/MainEquipos.xaml.cs
+++ Prop/MainEquipos.xaml.cs
@@ -22,10 +22,16 @@ namespace Prop
     {
         public int iIdTorneo;
         public string sNombreTorneo;
-
+        public List<Jugadores> items;
         public MainEquipos()
         {
-            InitializeComponent();            
+            InitializeComponent();
+            items = new List<Jugadores>();
+            /*items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+            items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });*/
+            items.Add(new Jugadores() { nombre = "Juanito", apellidoPaterno = "Rubez", apellidoMaterno = "Rodriguez", numero = 10 });
+            lvUsers.ItemsSource = items;
 
         }
 
@@ -75,18 +81,31 @@ namespace Prop
 
             if (idEquipo == 0)
             {
-                txtNombreEquipo.IsEnabled = true;
-                txtNombreEquipo.Focus();
+                HabilitaTxt(true);
             }
             else
-            {                
-
+            {
+                
             }
+        }
+
+        private void HabilitaTxt(bool bBabilita)
+        {
+            txtNombreEquipo.IsEnabled = bBabilita;
+
+            if (bBabilita)
+                txtNombreEquipo.Focus();
+
+            txtNombreJugador.IsEnabled = bBabilita;
+            txtApellidoMaterno.IsEnabled = bBabilita;
+            txApellidoPaterno.IsEnabled = bBabilita;
+            txtNumero.IsEnabled = bBabilita;
         }
 
         private void btnCreaEquipo_Click(object sender, RoutedEventArgs e)
         {
             AgregarEquipo();
+            AgregarJugadores();
         }
 
         public bool AgregarEquipo()
@@ -118,13 +137,45 @@ namespace Prop
             return bRegresa;
         }
 
+        public bool AgregarJugadores()
+        {
+            bool bRegresa = false;
+
+            using (var ctx = GetInstance())
+            {
+                foreach(Jugadores jugador in items)
+                { 
+                    string query = string.Format("INSERT INTO jugadores(nombre,paterno,materno,numero,idEquipo) VALUES" +
+                        "('{0}','{1}','{2}',{3},{4})", jugador.nombre,jugador.apellidoPaterno,jugador.apellidoMaterno,jugador.numero,1);
+
+                    using (var command = new SQLiteCommand(query, ctx))
+                    {
+
+                        if (command.ExecuteNonQuery() == 1)
+                        {
+                            string txtMensaje = string.Format("Se creo correctamente el equipo '{0}'", txtNombreEquipo.Text);
+                            MessageBox.Show(txtMensaje);
+                            bRegresa = true;
+                            cmbEquipos.ItemsSource = null;
+                            llenarCombo();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Algo salio mal al crear el nuevo torneo");
+                        }
+                    }
+                }
+            }
+
+            return bRegresa;
+        }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             llenarCombo();
-            llenarGrid();
+            //llenarGrid();
         }
 
-        public void llenarGrid()
+      /*  public void llenarGrid()
         {
             DataGridTextColumn dgtcNombre = new DataGridTextColumn();
             DataGridTextColumn dgtcPa = new DataGridTextColumn();
@@ -155,7 +206,7 @@ namespace Prop
             dgdEquipos.Columns.Add(dgtcNu);
 
             dgdEquipos.Items.Add(new Jugadores() { nombre = "Juanito", apellidoPaterno = "Rubez", apellidoMaterno="Rodriguez",numero= 10 });
-        }
+        }*/
 
         public class Jugadores
         {
@@ -163,6 +214,35 @@ namespace Prop
             public string apellidoPaterno { get; set; }
             public string apellidoMaterno { get; set; }            
             public int numero { get; set; }
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Mail { get; set; }
+        }
+
+        private void lvUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAgregarJugador_Click(object sender, RoutedEventArgs e)
+        {
+            lvUsers.ItemsSource = null;
+            items.Add(new Jugadores() { nombre = txtNombreJugador.Text, apellidoPaterno = txApellidoPaterno.Text, apellidoMaterno = txtApellidoMaterno.Text, numero = System.Convert.ToInt32(txtNumero.Text) });
+            lvUsers.ItemsSource = items;
+        }
+
+        private void cmbEquipos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(cmbEquipos.SelectedIndex == 0)
+                HabilitaTxt(false);
+
+
         }
 
         /*
